40baee5802fbc0ef620eb97efef1bfc48eb7a966
Matthew Eklund <107066346+mtthweklnd@users.noreply.github.com>
2025-09-07 08:33:01 -0700

    print method

--- R/constructor.R
+++ R/constructor.R
@@ -20,3 +20,51 @@ new_arcana <- function(name, orientation, keywords, number) {
     class = "arcana"
   )
 }
+
+#'Print Method for Arcana Objects
+#' A styled print method for arcana objects using the cli package.
+#'
+#' @param x An object of class "arcana"
+#' @param ... Additional arguments (unused)
+#'
+#' @return Invisibly returns the input object
+#'
+#' @importFrom cli make_ansi_style cli_div cli_h1 cli_text col_silver style_bold cli_ul
+#' @export
+print.arcana <- function(x, ...) {
+  name <- x$name
+  number <- x$number
+  roman <- if (number == 0) {
+    0
+  } else {
+    as.character(utils::as.roman(number))
+  }
+  orientation <- x$orientation
+  keywords <- keyword_vec <- trimws(strsplit(x$keywords, ",")[[1]])
+
+  color <- if (orientation == "upright") {
+    color <- "#0cc9ba"
+  } else {
+    color <- "#c91649"
+  }
+
+  card_theme <- list(
+    .box = list(
+      border = "single",
+      "border-col" = "grey",
+      padding = 1
+    ),
+    span.splash = list(color = color, "font-weight" = "bold"),
+    span.h3 = list(padding = 0)
+  )
+
+  cli::cli_div(id = "box", theme = card_theme)
+
+  cli_text()
+  cli_rule("{.strong {roman} - {name}}")
+  cli_text("── {.splash {toupper(orientation)}}")
+  cli_ul(keywords)
+  cli_end()
+
+  invisible(x)
+}
